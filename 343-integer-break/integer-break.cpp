ad39b57b2f7fa4fbec4c89f02222da08bdf7f1ad
nayanGowdaM <mnayangowda@gmail.com>
2023-10-06 12:54:38 +0530

    Time: 2 ms (49.96%) | Memory: 6.5 MB (29.03%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,34 +1,12 @@
 class Solution {
 public:
-    int find(int n, int k){
-        int ans=n/k,rem=n%k,res=1;
-        k=k-rem;
-        while(rem--) res*=(ans+1);
-        while(k--) res*=ans;
-        return res;
-    }
     int integerBreak(int n) {
-        if(n<3) return 1;
-        int l=2,r=n,al=find(n,l),ar=find(n,r);
-        cout<<al;
-        while((r-l)>1){
-            int m=(l+r)/2;
-            int am=find(n,m);
-            if(ar==al){
-                l++;
-                r--;
-                al=find(n,l);
-                ar=find(n,r);
-            }
-            if(al<ar) {
-                al=am;
-                l=m;
-            }
-            else{
-                ar=am;
-                r=m;
-            }
+        if(n<=3 ) return n-1;
+        int ans=1;
+        while(n>4){
+            ans*=3;
+            n-=3;
         }
-        return al>ar?al:ar;
+        return ans*n;
     }
 };
