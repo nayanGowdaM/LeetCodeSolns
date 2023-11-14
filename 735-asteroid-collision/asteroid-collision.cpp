2a7dd13d4f75dd0e019f0a8661ed7080af985e22
nayanGowdaM <mnayangowda@gmail.com>
2023-11-14 23:15:44 +0530

    Time: 5 ms (92.58%) | Memory: 18.2 MB (47.98%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -2,37 +2,23 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& a) {
         int n=a.size();
-        vector<int>pos(n,-1),neg(n,-1);
         stack<int> st;
-        for(int i=n-1;i>=0;i--){
-            if(a[i]>0){
-                if(!st.empty()){
-                    while(!st.empty() && a[i]>abs(st.top())) st.pop();
-                    if(!st.empty()){
-                        pos[i]=1;
-                        if(st.top()*-1 == a[i]) st.pop();
-                    }
+        for(int i=0;i<n;i++){
+            if(a[i]>0 || st.empty()) st.push(a[i]);
+            else{
+                while(!st.empty() && st.top()>0 && abs(a[i])>abs(st.top())) st.pop();
+                if(st.empty() || st.top()<0) st.push(a[i]);
+                else{
+                    if(abs(st.top())==abs(a[i])) st.pop();
                 }
             }
-            else st.push(a[i]);
         }
-        while(!st.empty()) st.pop();
-        for(int i=0;i<n;i++){
-            if(a[i]<0){
-                if(!st.empty()){
-                    while(!st.empty() && abs(a[i])>abs(st.top())) st.pop();
-                    if(!st.empty()){
-                        neg[i]=1;
-                        if(abs(st.top()) == abs(a[i])) st.pop();
-                    }
-                }
-            }
-            else st.push(a[i]);
+        vector<int> v;
+        while(!st.empty()) {
+            v.push_back(st.top());
+            st.pop();
         }
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            if(pos[i]==-1 && neg[i]==-1) ans.push_back(a[i]);
-        }
-        return ans;
+        reverse(v.begin(),v.end());
+        return v;
     }
 };
