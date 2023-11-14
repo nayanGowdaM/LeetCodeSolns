9f541ffb6e9b1a30c5b900ea7fb329cf00649097
nayanGowdaM <mnayangowda@gmail.com>
2023-11-14 12:19:35 +0530

    Time: 3 ms (13.84%) | Memory: 6.5 MB (36.03%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int countSegments(string s) {
-        vector<string>v;
-        int n=s.size();
-        for(int i=0;i<s.size();){
-            // string str="";
+        int n=s.size(),i=0,ans=0;
+        while(i<n){
             while(i<n && s[i]==' ') i++;
             if(i>=n) break;
-            int j=i;
-            while(j<n && s[j]!=' ') j++;
-            if(j<=n){
-                string str(s.begin()+i,s.begin()+j);
-                v.push_back(str);
-            }
-            i=j+1;
+            ans++;
+            while(i<n && s[i]!=' ')i++;
         }
-        return v.size();
+        return ans;
     }
 };
