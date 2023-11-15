2c7e13f1e6e7bd9e9d81a058ac1db5bd479bcb68
nayanGowdaM <mnayangowda@gmail.com>
2023-11-16 00:10:34 +0530

    Time: 31 ms (49.75%) | Memory: 11.7 MB (33.46%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,14 +1,49 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long ans=0,n=nums.size();
+        int n=nums.size();
+        long long ans=0;
+        vector<int> lm(n,-1),lmx(n,-1),rm(n,n),rmx(n,n);
+        stack<int> st;
         for(int i=0;i<n;i++){
-            int mx=INT_MIN, mn=INT_MAX;
-            for(int j=i;j<n;j++){
-                mx=max(mx,nums[j]);
-                mn=min(mn,nums[j]);
-                ans+=mx-mn;
-            }
+            while(!st.empty() && nums[st.top()]>=nums[i] )st.pop();
+            if(!st.empty())lm[i]=st.top();
+            st.push(i);
+        
+        }
+        while(!st.empty()) st.pop();
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<=nums[i] )st.pop();
+            if(!st.empty())lmx[i]=st.top();
+            st.push(i);
+        
+        }
+        while(!st.empty()) st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>nums[i] )st.pop();
+            if(!st.empty())rm[i]=st.top();
+            st.push(i);
+        
+        }
+        while(!st.empty()) st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<nums[i] )st.pop();
+            if(!st.empty())rmx[i]=st.top();
+            st.push(i);
+        
+        }
+        for(auto x: lm) cout<<x<<" ";
+        cout<<endl;
+        for(auto x: lmx) cout<<x<<" ";
+        cout<<endl;
+        for(auto x: rm) cout<<x<<" ";
+        cout<<endl;
+        for(auto x: rmx) cout<<x<<" ";
+        cout<<endl;
+        for(int i=0;i<n;i++){
+            long long m=(i-lm[i])*(rm[i]-i);
+            long long mx=(i-lmx[i])*(rmx[i]-i);
+            ans+=(mx-m)*nums[i];
         }
         return ans;
     }
