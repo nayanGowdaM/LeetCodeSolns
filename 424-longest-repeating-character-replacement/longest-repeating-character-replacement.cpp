4f6503ab78bb1ee8c791de5c74e3d2876045fe2d
nayanGowdaM <mnayangowda@gmail.com>
2023-11-18 13:25:50 +0530

    Time: 7 ms (80.83%) | Memory: 7.3 MB (92.77%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     
-    bool is_valid(string& s,vector<int>& hash , int i, int j, int k){
-        int n=s.size(),mx=*max_element(hash.begin(),hash.end());
-        return (j-i+1-mx)<=k?true:false;
+    bool is_valid(string& s,int maxF , int i, int j, int k){
+        // int n=s.size(),mx=*max_element(hash.begin(),hash.end());
+        return (j-i+1-maxF)<=k?true:false;
     }
     int characterReplacement(string s, int k) {
         vector<int> hash(26,0);
-        int n=s.size(),ans=0;
+        int n=s.size(),ans=0,maxF=0;
         for(int i=0,j=0;j<n;){
             // hash[s[j]-'A']++;
             // if(is_valid(s,i,j,k)){
@@ -21,7 +21,8 @@ public:
             //     }
             // }
             hash[s[j]-'A']++;
-            while(!is_valid(s,hash,i,j,k)){
+            maxF=max(maxF,hash[s[j]-'A']);
+            while(!is_valid(s,maxF,i,j,k)){
                 hash[s[i]-'A']--;
                 i++;
             }
