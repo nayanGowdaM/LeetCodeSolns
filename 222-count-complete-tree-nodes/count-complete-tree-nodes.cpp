f7d1c8c135879796817924efa485acab549b0f57
nayanGowdaM <mnayangowda@gmail.com>
2023-11-28 19:19:03 +0530

    Time: 19 ms (92.15%) | Memory: 31.1 MB (50.01%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -13,6 +13,11 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
         if(!root) return 0;
+        int l=0,r=0;
+        TreeNode *left=root,*right=root;
+        while(left){ left=left->left;l++;}
+        while(right) {right=right->right;r++;}
+        if(l==r) return pow(2,l)-1;
         return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
