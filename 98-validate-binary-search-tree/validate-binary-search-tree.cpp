caec15c86ecf4c83de07ca3ed169ab59deb2521d
nayanGowdaM <mnayangowda@gmail.com>
2023-12-01 19:40:09 +0530

    Time: 7 ms (77.41%) | Memory: 21.9 MB (85.39%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,22 +11,12 @@
  */
 class Solution {
 public:
-    int leftMost(TreeNode* root){
-        while(root->left) root=root->left;
-        return root->val;
-    }
-
-    int rightMost(TreeNode* root){
-        while(root->right) root=root->right;
-        return root->val;
+    bool isValid(TreeNode* root, pair<long long ,long long > p){
+        if(!root ) return true;
+        return root->val<p.second && root->val>p.first && isValid(root->left, {p.first,root->val}) && isValid(root->right, {root->val, p.second});
     }
     bool isValidBST(TreeNode* root) {
-        if(!root || !root->left && !root->right) return true;
-        if((root->left && root->left->val >= root->val) || ( root->right  && root->right->val <= root->val)) return false;
-        if(root->right && root->left)
-        return isValidBST(root->left)&& isValidBST(root->right) && (root->val<leftMost(root->right))&& (root->val>rightMost(root->left));
-        else if(root->left)return isValidBST(root->left)  && (root->val>rightMost(root->left));
-        else if(root->right) return isValidBST(root->right) && (root->val<leftMost(root->right));
-        else return true;
+        if(!root || !root->left && !root->right) return  true;
+        return isValid(root, {LLONG_MIN,LLONG_MAX});
     }
 };
