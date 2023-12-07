629ed3f8c07cec5dee1d4f407b8eef3532f31b72
nayanGowdaM <mnayangowda@gmail.com>
2023-12-07 18:31:11 +0530

    Time: 56 ms (5.41%) | Memory: 37.2 MB (46.61%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -9,16 +9,64 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ class BSTI{
+   stack<TreeNode* > stl,str;
+   void helpl(TreeNode* root) {
+     while(root){
+       stl.push(root);
+       root=root->left;
+     }
+   }
+   void helpr(TreeNode* root){
+     while(root){
+       str.push(root);
+       root=root->right;
+     }
+   }
+   public:
+   BSTI(TreeNode* root){
+     helpl(root);
+     helpr(root);
+   }
+   int next(){
+     TreeNode* node=stl.top();
+     stl.pop();
+     helpl(node->right);
+     return node->val;
+   }
+   int prev(){
+     TreeNode* node=str.top();
+     str.pop();
+     helpr(node->left);
+     return node->val;
+   }
+   bool hasnext(){
+     return !stl.empty();
+   }
+   bool hasprev(){
+     return !str.empty();
+   }
+ };
 class Solution {
 public:
-    bool find(TreeNode* root, int k , unordered_set<int>& mp){
-      if(!root) return false;
-      if(mp.find(k-root->val)!=mp.end()) return true;
-      mp.insert(root->val);
-      return find(root->left,k,mp)||find(root->right,k,mp);
-    }
     bool findTarget(TreeNode* root, int k) {
-        unordered_set<int> mp;
-        return find(root, k, mp);
+        BSTI* x=new BSTI(root);
+        int a=x->next(),b=x->prev();
+        cout<<a<<" "<<b<<" ";
+        while(x->hasnext() || x->hasprev()){
+          int sum=a+b;
+          if(sum==k && a!=b) return true;
+          if(sum<k){
+            if(x->hasnext()) a=x->next();
+            else return false;
+          }
+          else{
+            if(x->hasprev()) b=x->prev();
+            else return false;
+          }
+          cout<<a<<" "<<b<<" ";
+        }
+        return false;
     }
 };
