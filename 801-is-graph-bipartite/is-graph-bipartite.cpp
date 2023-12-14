ccc6a0533236310ecd86d4981ea14ae57edebef5
nayanGowdaM <mnayangowda@gmail.com>
2023-12-14 22:43:42 +0530

    Time: 19 ms (67.13%) | Memory: 13.9 MB (52.31%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,19 +1,26 @@
 class Solution {
-    bool dfs(unordered_set<int>& s1,unordered_set<int>& s2, vector<vector<int>>& a,vector<int>& vis, int src){
-        vis[src]=1;
-        for(auto x: a[src]){
-            if(!vis[x] && !dfs(s2,s1,a,vis,x)) return false;
-            if(s1.find(x)!=s1.end()) return false;
-            s2.insert(x);
+    bool bfs(vector<vector<int>>& a, vector<int>& vis, int src){
+        queue<int> q;
+        q.push(src);
+        vis[src]=0;
+        while(!q.empty()){
+            int cur=q.front();
+            q.pop();
+            for(int x: a[cur]){
+                if(vis[x]==-1){
+                    q.push(x);
+                    vis[x]=!vis[cur];
+                }
+                else if(vis[x]==vis[cur]) return false;
+            }
         }
         return true;
     }
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        unordered_set<int>s1,s2;
-        vector<int> vis(graph.size(),0);
-        for(int i=0;i<graph.size();i++){
-            if(!vis[i] && !dfs(s1,s2,graph,vis,i)) return false;
+    bool isBipartite(vector<vector<int>>& a) {
+        vector<int>vis(a.size(),-1);
+        for(int i=0;i<a.size();i++){
+            if(vis[i]==-1 && !bfs(a,vis, i)) return false; 
         }
         return true;
     }
