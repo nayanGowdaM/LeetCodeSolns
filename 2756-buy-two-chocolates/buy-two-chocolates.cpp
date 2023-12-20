98c2052cc6312d0f49e40e0b6304d5d97aff7fb7
nayanGowdaM <mnayangowda@gmail.com>
2023-12-20 12:46:56 +0530

    Time: 18 ms (23.07%) | Memory: 47.1 MB (13.41%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int buyChoco(vector<int>& a, int money) {
-        int idx=min_element(a.begin(),a.end())-a.begin();
-        int m=INT_MAX;
-        for(int i=0;i<a.size();i++){
-            if(idx!=i){
-                m=min(m,a[i]);
-            }
+        priority_queue<int> pq;
+        for(auto x: a){
+            pq.push(x);
+            if(pq.size()>2) pq.pop();
         }
-        int ans=a[idx]+m;
-        cout<<a[idx]<<" "<<m;
-        if(ans<=money) return money-ans;
-        else return money;
+        int ans=pq.top();
+        cout<<pq.top();
+        pq.pop();
+        ans+=pq.top();
+        cout<<pq.top();
+        cout<<ans;
+        return ans<=money? money-ans: money;
     }
 };
