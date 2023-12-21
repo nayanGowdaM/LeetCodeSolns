4d25c17eb215a5a98b2ace4703c53fccfaaf0507
nayanGowdaM <mnayangowda@gmail.com>
2023-12-21 19:58:25 +0530

    Time: 111 ms (39.23%) | Memory: 55.6 MB (23.82%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,50 +1,28 @@
-
-class Disjoint{
-    
-    public:
-    vector<int> par, rank;
-    Disjoint(int n){
-        par.resize(n);
-        rank.resize(n,0);
-        for(int i=0;i<n;i++) par[i]=i;
-    }
-
-    int findParent(int node){
-        if(node==par[node]) return node ;
-        return par[node]=findParent(par[node]);
-
-    }
-
-    void Union(int u,int v){
-        int upU=findParent(u),upV=findParent(v);
-        if(upU==upV) return;
-        if(rank[upU] < rank[upV] )  par[upU] =upV;
-        else if(rank[upV] < rank[upU]) par[upV]=upU;
-        else{
-            par[upU]=upV;
-            rank[upV]++;
+class Solution {
+    void dfs(int src, vector<int> adj[],vector<int>& vis){
+        vis[src]=1;
+        int n=vis.size();
+        for(auto i: adj[src]){
+            if(!vis[i]) dfs(i,adj,vis);
         }
     }
-};
-
-class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& conn) {
-        Disjoint ds(n);
+        int edges=conn.size();
+        if(edges<n-1) return -1;
+        vector<int>adj[n];
         for(auto x: conn){
-            ds.Union(x[0],x[1]);
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
         }
-        unordered_map<int,int>mp;
+        int disjointSet=0;
+        vector<int>vis(n,0);
         for(int i=0;i<n;i++){
-            int par=ds.findParent(i);
-            mp[par]=(mp.find(par)==mp.end())?0:mp[par]+1;
+            if(!vis[i]){
+                dfs(i,adj,vis);
+                disjointSet++;
+            }
         }
-        int disjointSet=mp.size(),reqEdges=0;
-        for(auto& x: mp){
-            reqEdges+=x.second;
-        }
-        int totalEdges=conn.size();
-        if(totalEdges-reqEdges>=disjointSet-1) return disjointSet-1;
-        else return -1;
+        return disjointSet-1;
     }
 };
