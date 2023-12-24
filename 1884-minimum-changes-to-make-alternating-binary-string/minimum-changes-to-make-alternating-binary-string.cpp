da6c9d38b8066dc0fc4d285555fa7971565692d1
nayanGowdaM <mnayangowda@gmail.com>
2023-12-24 19:32:55 +0530

    Time: 8 ms (8.31%) | Memory: 7.5 MB (24.33%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        char z='0',o='1';
+        int ct1=0,ct2=0;
+        for(int i=0;i<s.size();i++){
+            if(i%2){
+                if(s[i]!=o) ct1++;
+            }
+            else{
+                if(s[i]!=z) ct1++;
+            }
+        }
+        for(int i=0;i<s.size();i++){
+            if(i%2==0){
+                if(s[i]!=o) ct2++;
+            }
+            else{
+                if(s[i]!=z) ct2++;
+            }
+        }
+        return min(ct1,ct2);
+    }
+};
