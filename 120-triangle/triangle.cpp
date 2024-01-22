5bac95f24b5974751ac7ba464a8e08b690e2f887
nayanGowdaM <mnayangowda@gmail.com>
2024-01-23 01:53:48 +0530

    Time: 3 ms (89.23%) | Memory: 10.8 MB (7.15%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,13 +1,13 @@
 class Solution {
-    int dp[202][202];
-    int solve(int m,int n, int l, vector<vector<int>>& a){
-        if(m==l) return a[m][n];
-        if(dp[m][n]!=-1) return dp[m][n];
-        return dp[m][n]=a[m][n]+min(solve(m+1,n,l,a),solve(m+1,n+1,l,a));
-    }
 public:
     int minimumTotal(vector<vector<int>>& a) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,a.size()-1,a);
+        int m=a.size();
+        for(int i=m-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                a[i][j]=a[i][j]+min(a[i+1][j],a[i+1][j+1]);
+            }
+        }
+        return a[0][0];
+
     }
 };
