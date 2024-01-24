a0d50398ac5e8a7477c3f5bd6c9f7402840c0919
nayanGowdaM <mnayangowda@gmail.com>
2024-01-25 00:23:52 +0530

    Time: 281 ms (56.38%) | Memory: 16 MB (60.49%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,31 +1,23 @@
 class Solution {
-
-    bool fun(vector<int>& nums, int sum,int n, vector<vector<int>>& dp){
-        if(sum==0) return dp[n][sum]=true;
-        if(n==0) return false;
-        if(dp[n][sum]!=-1) return dp[n][sum];
-        if(nums[n-1]<=sum) return dp[n][sum] = fun(nums,sum-nums[n-1],n-1,dp) || fun(nums,sum,n-1,dp);
-        else return dp[n][sum] = fun(nums,sum,n-1,dp);
-    }
-
 public:
+
     bool canPartition(vector<int>& nums) {
-        int sum=accumulate(nums.begin(),nums.end(),0),n=nums.size();
-        
-        if(sum%2) return false;
-        vector<vector<int>>dp(n+1,vector<int>(sum/2+1,-1));
-        // return  fun(nums,sum/2,n,dp);
-        sum=sum/2;
-        for(int i=0;i<=sum;i++) dp[0][i]=0;
-        for( int i=0;i<=n;i++) dp[i][0]=1;
-        for( int i=1;i<=n;i++){
-            for( int  j =1 ; j<=sum;j++){
-                if(nums[i-1]<=j) dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
+        int s=accumulate(nums.begin(),nums.end(),0);
+        if(s & 1) return false;
+        s=s/2;
+        int n=nums.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(s+1,false));
+        for(int i=0;i<=n;i++) dp[i][0]=true;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=s;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j]= dp[i-1][j-nums[i-1]] || dp[i-1][j];
+
+                }
                 else dp[i][j]=dp[i-1][j];
             }
         }
-        return dp[n][sum];
+        return dp[n][s];
 
-        
     }
 };
