67227e20f50142fe97f40d05fb4d92aa21896a57
nayanGowdaM <mnayangowda@gmail.com>
2024-01-30 01:54:40 +0530

    Time: 53 ms (65.61%) | Memory: 14.9 MB (22.12%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,35 +1,14 @@
-int dp[2502][2502];
-
 class Solution {
-    int solve(int n, int next, vector<int>& arr){
-        if(n<0) return 0;
-        // if(next==arr.size()) return max( 1 + solve(n-1,n, arr) , solve(n-1, next, arr));
-        if(dp[n][next]!=-1) return dp[n][next];
-        if(next == arr.size() || arr[next] > arr[n]) return dp[n][next] = max( 1 + solve(n-1,n, arr) , solve(n-1, next, arr));
-        else return dp[n][next] = solve(n-1,next, arr);
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        // memset(dp, -1, sizeof(dp));
-        // return solve(nums.size()-1,nums.size(),nums);
-
-
-        int n=nums.size();
-        // vector<vector<int>> dp(n+1, vector<int> ( n+2, 0));
-        vector<int> cur(n+2,0),prev(n+2,0);
-        for( int i=1 ; i<=n;i++){
-            for( int j=i+1; j<=n+1;j++){
-                int len= prev[j];
-                if( j==n+1 || nums[j-1]> nums[i-1]) len=max(len, 1+ prev[i]);
-                cur[j]=len;
-                // cout<<len<<" ";
-            }
-            prev=cur;
-            // cout<<endl;
-            
+        set<pair<int,int>> st;
+        st.insert( {-1,nums[0]});
+        for( int i=1;i<nums.size(); i++){
+            auto it=st.begin();
+            while( it!= st.end() && it->second>=nums[i]) it++;
+            if( it==st.end()) st.insert({-1,nums[i]});
+            else st.insert({it->first-1, nums[i]}); 
         }
-
-        return cur[n+1];
-
+        return -st.begin()->first ;
     }
 };
