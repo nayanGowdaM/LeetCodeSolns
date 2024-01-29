16a07ce8d51cb757e03a070e5759551467bc133e
nayanGowdaM <mnayangowda@gmail.com>
2024-01-29 23:29:19 +0530

    Time: 68 ms (95.45%) | Memory: 57.1 MB (70.16%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -18,25 +18,26 @@ class Solution {
     //     return dp[n][sold] = profit;
     // }
 public:
-    int maxProfit(vector<int>& st, int fee) {
+    int maxProfit(vector<int>& st,int fee) {
         // memset(dp,-1,sizeof(dp));
         // int n=st.size();
         // return solve(n, st, 0);
 
 
         int n=st.size();
-        vector<vector<int>> dp(n+1,vector<int>(3,-1));
-        dp[n][0]=dp[n][1]=0;
+        // vector<vector<int>> dp(n+1,vector<int>(3,-1));
+        vector<int> cur(3,-1),next(3,0);
         int buy=0;
         for(int i=n-1;i>=0;i--){
             for(int buy=0;buy<2;buy++){
                 if(buy){
-                    dp[i][buy] = max( -st[i] + dp[i+1][0] , dp[i+1][1]);
+                    cur[buy] = max( -st[i] + next[0] , next[1]);
                     
                 } 
-                else dp[i][buy] = max( st[i]-fee + dp[i+1][1] , dp[i+1][0]);
+                else cur[buy] = max( st[i] - fee + next[1] , next[0]);
             }
+            next=cur;
         } 
-        return dp[0][1];
+        return cur[1];
     }
 };
