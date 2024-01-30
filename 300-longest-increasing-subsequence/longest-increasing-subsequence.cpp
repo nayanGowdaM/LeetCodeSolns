5eba947c1046c4bd81d98ea5c9eee276ea8a951b
nayanGowdaM <mnayangowda@gmail.com>
2024-01-30 16:52:35 +0530

    Time: 3 ms (97.93%) | Memory: 12.8 MB (25.89%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        set<pair<int,int>> st;
-        st.insert( {-1,nums[0]});
-        for( int i=1;i<nums.size(); i++){
-            auto it=st.begin();
-            while( it!= st.end() && it->second>=nums[i]) it++;
-            if( it==st.end()) st.insert({-1,nums[i]});
-            else st.insert({it->first-1, nums[i]}); 
+        vector<int> ans;
+        for( auto& x: nums){
+            auto it= lower_bound(ans.begin(),ans.end(),x) - ans.begin();
+            if(it==ans.size()) ans.push_back(x);
+            else
+            ans[it]=x;
         }
-        return -st.begin()->first ;
+        return ans.size();
     }
 };
