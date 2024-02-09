689dd1c5b493b50d9426bc214259c853f3764ea1
nayanGowdaM <mnayangowda@gmail.com>
2024-02-09 15:13:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (7.28%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -8,7 +8,15 @@ class Solution {
     }
 public:
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return rob(nums,nums.size()-1);
+
+         int n=nums.size();
+         if(n==1) return nums[0];
+         vector<int>dp(n+1);
+         dp[0] = nums[0];
+         dp[1] = max( nums[0] , nums[1]);
+         for( int i=2;i<n;i++){
+             dp[i] = max( nums[i] + dp[i-2] , dp[i-1]);
+         }
+         return dp[n-1];
     }
 };
