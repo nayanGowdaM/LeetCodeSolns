c756fc261fd1911d1f966a27cd9a2e93e4bd9b43
nayanGowdaM <mnayangowda@gmail.com>
2024-02-10 01:08:21 +0530

    Time: 5 ms (77.95%) | Memory: 11.9 MB (38.15%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,29 +1,14 @@
 class Solution {
-    int dp[202][202];
-    int solve(vector<vector<int>>& a,int m,int n){
-        if(m==0 && n==0) return a[m][n];
-        int ct1=INT_MAX,ct2=INT_MAX;
-        if(dp[m][n]!=-1) return dp[m][n];
-        if(n) ct1=dp[m][n-1]=solve(a,m,n-1);
-        if(m) ct2=dp[m-1][n]=solve(a,m-1,n);
-        return dp[m][n]=a[m][n]+ min(ct1,ct2);
-    }
 public:
-    int minPathSum(vector<vector<int>>& a) {
-        int m=a.size(),n=a[0].size();
-        int dp[m+1][n+1];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[i][j]=a[i][j];
-                else {
-                    if(j && i) dp[i][j]=a[i][j] + min(dp[i-1][j],dp[i][j-1]);
-                    else if(j) dp[i][j]=a[i][j]+dp[i][j-1];
-                    else dp[i][j]=a[i][j]+dp[i-1][j];
-                }
+    int minPathSum(vector<vector<int>>& dp) {
+        int m=dp.size(), n=dp[0].size();
+        for( int i=1;i<m;i++) dp[i][0]+=dp[i-1][0];
+        for( int j=1;j<n;j++) dp[0][j]+=dp[0][j-1];
+        for( int i=1;i<m;i++){
+            for(  int j=1;j<n;j++){
+                dp[i][j] += min(dp[i-1][j],dp[i][j-1]);
             }
         }
         return dp[m-1][n-1];
-
-
     }
 };
