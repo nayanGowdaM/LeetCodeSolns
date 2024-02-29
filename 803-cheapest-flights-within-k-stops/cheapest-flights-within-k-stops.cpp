ac4aedf7790c582d18a034a73159394fe17f612c
nayanGowdaM <mnayangowda@gmail.com>
2024-02-29 17:31:58 +0530

    Time: 17 ms (50.15%) | Memory: 17.1 MB (34.84%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,32 +1,32 @@
-typedef pair<pair<int,int> , int> ppi;
+
+
 class Solution {
+    typedef pair<pair<int,int>,int> ppi;
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& f, int src, int dest, int k) {
-        vector<pair<int,int>> adj[n];
-        for(auto x: f){
-            adj[x[0]].push_back({x[1],x[2]});
-        }
-        priority_queue<ppi, vector<ppi>, greater<ppi>>q;
-        vector<int>cost(n,INT_MAX);
-        cost[src]=0;
-        q.push({{0,0},src});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        priority_queue<ppi, vector<ppi> , greater<ppi>>pq;
         int ans=INT_MAX;
-        while(!q.empty()){
-            int vis=q.top().first.first, c=q.top().first.second , node=q.top().second;
-            q.pop();
-            if(dest==node) ans=min(ans,c) ;
-           
-                for(auto x: adj[node]){
-                    if(c+x.second < cost[x.first] && vis<=k){
-                        cost[x.first]=c+x.second;
-                        q.push({{vis+1,cost[x.first]},x.first});
-                    }
-                }
-            
+        vector<pair<int,int>> adj[n];
+        for( auto x: flights){
+            adj[x[0]].push_back({x[1], x[2]});
         }
-        for(auto x: cost) cout<<x<< " ";
-        if(ans==INT_MAX)
-        return -1;
-        return ans;
+        vector<int> cost(n, INT_MAX);
+        cost[src]=0;
+        pq.push({{0,0}, src});
+        while(!pq.empty()){
+            int vis=pq.top().first.first, dist=pq.top().first.second, node=pq.top().second;
+            pq.pop();
+            if(vis<=k){
+                for( auto x :adj[node] ){
+                    int len=dist + x.second;
+                    if(len < cost[x.first]){
+                        cost[x.first]=len ;
+                        pq.push({{vis+1,len},x.first});
+                    }
+                    
+                }
+            }
+        }
+        return cost[dst]==INT_MAX? -1:cost[dst];
     }
 };
