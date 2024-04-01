99a43a636abd738cc65c97f831005097b1390ac0
nayanGowdaM <mnayangowda@gmail.com>
2024-04-01 09:58:41 +0530

    Time: 4 ms (47.87%) | Memory: 7.7 MB (57.50%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.size()-1;
+        int ans=0;
+        while( s[i]==' ') i--;
+        while(i>=0 &&  s[i]!=' '){
+            i--;
+            ans++;
+        }
+        return ans;
+    }
+};
