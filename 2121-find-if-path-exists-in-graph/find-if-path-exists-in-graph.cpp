dc29d3a6322157a207a20f706baf9efbd29c6557
nayanGowdaM <mnayangowda@gmail.com>
2024-04-22 00:13:22 +0530

    Time: 325 ms (85.52%) | Memory: 140.2 MB (84.30%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,20 +1,26 @@
 class Solution {
-    bool dfs( int src, vector<int> adj[], int dest, vector<int>& vis){
-        if( src == dest) return true;
-        vis[src]= 1;
-        for( auto x : adj[src]){
-            if( !vis[x] && dfs( x, adj, dest, vis)) return true;
+    int find_parent( int node, vector<int>& par, vector<int>& rank){
+        if(node == par[node] ) return node;
+        else return par[node]= find_parent(par[node], par, rank);
+    }
+
+    void Union( int u, int v, vector<int>& par, vector<int>& rank){
+        int up_u=find_parent( u, par, rank), up_v=find_parent( v, par, rank);
+        if( up_u == up_v) return;
+        else if( rank[up_u] > rank[up_v]) par[up_v] = up_u;
+        else if(rank[up_v] > rank[up_u]) par[up_u] = up_v;
+        else {
+            par[up_v] = up_u;
+            rank[up_u]++;
         }
-        return false;
     }
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<int> adj[n];
-        for( auto x: edges){
-            adj[x[0]].push_back(x[1]);
-            adj[x[1]].push_back(x[0]);
+    bool validPath(int n, vector<vector<int>>& edges, int src, int dest) {
+        vector<int>par(n+1), rank(n+1,0);
+        for(int i=0;i<=n;i++) par[i] = i;
+        for(auto x: edges){
+            Union( x[0], x[1], par, rank);
         }
-        vector<int> vis(n,0);
-        return dfs(source, adj, destination , vis);
+        return find_parent(src, par, rank) == find_parent( dest, par, rank);
     }
 };
