b19aae3fd0733c01fb306a25fef1a909308da17e
nayanGowdaM <mnayangowda@gmail.com>
2024-04-24 11:03:24 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (81.41%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,18 +1,15 @@
 class Solution {
-    int solve( int n){
-        if(n==0) return 0;
-        if(n==1 || n==2) return 1;
-        return solve( n-1)+ solve( n-2 ) + solve(n-3);
-    }
 public:
     int tribonacci(int n) {
-        // return solve(n);
-        if(n==0) return 0;
-        if(n==1 || n==2) return 1;
-        vector<int> dp(n+1);
-        dp[0] = 0;
-        dp[1] = dp[2] = 1;
-        for( int i=3;i<=n;i++) dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-        return dp[n];
+        if( n==0) return 0;
+        int t0 = 0, t1 = 1, t2=1;
+        int ans;
+        for ( int i=3;i<=n;i++){
+            ans = t0+t1+t2;
+            t0=t1;
+            t1=t2;
+            t2=ans;
+        }
+        return t2;
     }
 };
