da51d6b17ddf75bda2f22323f0f143da32894e8f
nayanGowdaM <mnayangowda@gmail.com>
2024-05-03 13:03:44 +0530

    Time: 4 ms (76.04%) | Memory: 11.4 MB (18.75%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,17 +1,9 @@
 class Solution {
-    int isSubstring(string& s1, string& s2)
-{
-    // using find method to check if s1 is
-    // a substring of s2
-    if (s2.find(s1) != string::npos)
-        return s2.find(s1);
-    return -1;
-}
 public:
     int numOfStrings(vector<string>& patterns, string word) {
-        int ans = 0;
-        for( auto x: patterns){
-            ans += ( isSubstring(x, word)!=-1);
+        int ans=0;
+        for( auto x: patterns ){
+            ans += ( word.find(x)!=string::npos);
         }
         return ans;
     }
