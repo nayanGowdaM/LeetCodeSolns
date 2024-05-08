c44d36ddf7d7fe792152febd957849189fd79514
nayanGowdaM <mnayangowda@gmail.com>
2024-05-08 20:51:54 +0530

    Time: 159 ms (31.51%) | Memory: 290.9 MB (11.32%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,33 +1,63 @@
 class Solution {
-public:
-    string longestPalindrome(string s) {
-        int ct=0;
-        int n=s.size();
-        string ans="";
-        for( int i=0;i<n;i++){
-            int l=i, r=i;
-            while(l>=0 && r<n && s[l]==s[r]){
-                l--;
-                r++;
-            }
-            if(r-l-1 > ct){
-                ct=r-l-1;
-                ans=s.substr(l+1, r-l-1);
-            }
+    int x=-1,y=-1;
 
-            l=i;
-            r=i+1;
-            while( l>=0 && r<n && s[l]==s[r]){
-                l--;
-                r++;
+    bool solve(string& s, int i, int j){
+        if( s[i]==s[j]){
+            if( j-i <=2 ){
+                if(y-x < j-i){
+
+                    y=j;
+                    x=i;
+                }
+                return true;
             }
-            if(r-l-1 > ct){
-                ct=r-l-1;
-                ans=s.substr(l+1, r-l-1);
+            else{
+                if( solve(s, i+1, j-1)){
+                    if(y-x < j-i){
+
+                        y=j;
+                        x=i;
+                    }
+                    return true;
+                }
+                else return false;
             }
-            
+                
 
         }
-        return ans;
+        else{
+            return solve( s, i+1, j) || solve(  s, i, j-1);
+        }
+    }
+public:
+    string longestPalindrome(string s) {
+        int n = s.size(), x=-1,y=-1, len=0;
+        vector<vector<int>>dp( n+1, vector<int>( n+1, 0));
+        for( int i=n-1;i>=0;i--){
+            for( int j=i;j<n;j++){
+                if( s[i] == s[j]){
+                    if( j-i <= 2) dp[i][j]=j-i+1;
+                    else if( dp[i+1][j-1]){
+                        dp[i][j] = dp[i+1][j-1] + 2 ;
+                    }
+                    else dp[i][j]=0;
+
+                    if(len < dp[i][j]){
+                        len = dp[i][j];
+                        x = i;
+                        y = j;
+                    }
+                }
+                else dp[i][j] = 0;
+            }
+        }
+
+        // for( int i=0;i<n;i++){
+        //     for( int j=0;j<n;j++) cout<<dp[i][j] <<" ";
+        //     cout<<endl;
+        // }
+        return s.substr( x, y-x+1);
+
+
     }
 };
