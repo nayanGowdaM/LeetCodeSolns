d3cdb054fe0a57d7681fcf2bf4e390db83322f03
nayanGowdaM <mnayangowda@gmail.com>
2024-05-08 20:56:29 +0530

    Time: 102 ms (41.74%) | Memory: 79.1 MB (36.89%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -10,15 +10,15 @@ class Solution {
         return true;
     }
 
-    void solve( string& s, int n, vector<string>& res){
+    void solve( string& s, int n, vector<string>& res , vector<vector<bool>>& dp){
         if( n==s.size()){
             ans.push_back(res);
             return;
         }
         for( int i=n+1;i<=s.size();i++){
-            if( isPal( s, n, i-1)){
+            if( dp[n][i-1]){
                 res.push_back(s.substr( n, i-n));
-                solve( s, i, res);
+                solve( s, i, res, dp);
                 res.pop_back();
             }
         }
@@ -26,7 +26,19 @@ class Solution {
 public:
     vector<vector<string>> partition(string s) {
         vector<string>res;
-        solve( s, 0, res);
+        int n=s.size();
+        vector<vector<bool>> dp( n, vector<bool>(n,false));
+        for( int i=n-1;i>=0 ;i--){
+            for(int j=i;j<n;j++){
+                if( s[i]==s[j]){
+                    if( j-i <= 2) dp[i][j] = true;
+                    else dp[i][j] = dp[i+1][j-1];
+                }
+                else dp[i][j]=false;
+                
+            }
+        }
+        solve( s, 0, res, dp);
         return ans;
     }
 };
