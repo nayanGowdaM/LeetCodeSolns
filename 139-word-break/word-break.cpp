294b7da2199ea6b3a6cc84744ebee92c5fb96e27
nayanGowdaM <mnayangowda@gmail.com>
2024-05-09 20:27:56 +0530

    Time: 18 ms (28.39%) | Memory: 18.4 MB (18.69%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,17 +1,25 @@
-int dp[302][302];
-
 class Solution {
-    bool solve( int idx, int n, string& s, vector<string>& dict){
-        if( idx==-1) return n==1? true: false;
-        if( dp[idx][n]!=-1) return dp[idx][n];
-        bool x = solve( idx-1,n+1, s,dict), y=false ;
-        if( find( dict.begin(),dict.end(), s.substr(idx, n))!=dict.end()) 
-        y= solve(  idx-1,1,s,dict);
-        return dp[idx][n] = x||y;
+    int dp[302][302];
+    bool present( string s, vector<string>& dict){
+        auto it = lower_bound( dict.begin(), dict.end(), s);
+        if( it== dict.end()) return false;
+        else return *it==s;
+    }
+
+    bool solve( string& s, int start, int end, vector<string>& dict){
+        if( start == s.size()) return true;
+        if( end == s.size() ) return false;
+        if(dp[start][end] != -1) return dp[start][end];
+        // cout<<start<<" "<<end<<endl;
+
+        if(  present(s.substr( start , end-start+1) , dict )  && solve(s, end+1, end+1, dict)) return dp[start][end] = 1;
+        if(solve( s, start, end+1, dict) ) return dp[start][end] = true;
+        return dp[start][end] = 0;
     }
 public:
     bool wordBreak(string s, vector<string>& dict) {
-        memset(  dp, -1, sizeof(dp));
-        return solve( s.size()-1, 1,s, dict);
+        memset(dp,-1, sizeof(dp) );
+        sort( dict.begin(), dict.end());
+        return solve( s, 0,0,dict);
     }
 };
