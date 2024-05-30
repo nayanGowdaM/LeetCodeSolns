3fe2955f840cd8a53f1cff3da9392e561c9dd419
nayanGowdaM <mnayangowda@gmail.com>
2024-05-30 19:20:52 +0530

    Time: 38 ms (20.17%) | Memory: 38.5 MB (22.49%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -9,64 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
- class BSTI{
-   stack<TreeNode* > stl,str;
-   void helpl(TreeNode* root) {
-     while(root){
-       stl.push(root);
-       root=root->left;
-     }
-   }
-   void helpr(TreeNode* root){
-     while(root){
-       str.push(root);
-       root=root->right;
-     }
-   }
-   public:
-   BSTI(TreeNode* root){
-     helpl(root);
-     helpr(root);
-   }
-   int next(){
-     TreeNode* node=stl.top();
-     stl.pop();
-     helpl(node->right);
-     return node->val;
-   }
-   int prev(){
-     TreeNode* node=str.top();
-     str.pop();
-     helpr(node->left);
-     return node->val;
-   }
-   bool hasnext(){
-     return !stl.empty();
-   }
-   bool hasprev(){
-     return !str.empty();
-   }
- };
 class Solution {
+    bool twoSum( TreeNode* root, int k, unordered_set<int>& st){
+        if( !root) return false;
+        if( st.find( k-root->val) !=st.end()) return true;
+        st.insert( root->val);
+        return twoSum( root->left, k,st) || twoSum( root->right, k, st);
+    }
 public:
     bool findTarget(TreeNode* root, int k) {
-        BSTI* x=new BSTI(root);
-        int a=x->next(),b=x->prev();
-        cout<<a<<" "<<b<<" ";
-        while(x->hasnext() || x->hasprev()){
-          int sum=a+b;
-          if(sum==k && a!=b) return true;
-          if(sum<k){
-            if(x->hasnext()) a=x->next();
-            else return false;
-          }
-          else{
-            if(x->hasprev()) b=x->prev();
-            else return false;
-          }
-          cout<<a<<" "<<b<<" ";
-        }
-        return false;
+        unordered_set<int> st;
+        return twoSum( root, k, st);
     }
 };
