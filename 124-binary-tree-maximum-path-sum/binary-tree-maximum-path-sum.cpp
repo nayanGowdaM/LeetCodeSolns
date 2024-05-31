afe9c57e1e00464ae3470b840bd2c5704f1368e0
nayanGowdaM <mnayangowda@gmail.com>
2024-05-31 18:26:52 +0530

    Time: 21 ms (35.63%) | Memory: 26 MB (95.43%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -10,27 +10,19 @@
  * };
  */
 class Solution {
-    int ans=INT_MIN;
-    int solve(TreeNode* root){
-        if(!root ) return 0;
-        // if(!root->left && !root->right) return root->val;
-        int left = max(0, solve(root->left));
-        int right = max(0, solve(root->right));
-        ans = max( ans,left + right + root->val);
-        return root->val + max( left, right) ;
-    }
-    int maxDown(TreeNode* root, int &ans){
-        if(!root) return 0;
-        int l=max(0,maxDown(root->left,ans));
-        int r=max(0,maxDown(root->right,ans));
-        ans=max(ans,l+r+root->val);
-        return max(r,l)+root->val;
+    int ans = -1001;
+
+    int solve( TreeNode* root){
+        if(!root) return   0;
+        int left = max( 0, solve(root->left));
+        int right = max( 0, solve( root->right));
+        ans = max( ans, left + right + root->val);
+        return root->val + max( left, right);
+
     }
 public:
     int maxPathSum(TreeNode* root) {
-        return  max(ans, solve(root));
+        int res = solve( root);
+        return  max( ans, res);
     }
 };
-
-
-
