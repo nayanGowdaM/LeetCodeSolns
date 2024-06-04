af0adb8520ceab229c4c4e836ba39e5e642b10cc
nayanGowdaM <mnayangowda@gmail.com>
2024-06-05 00:55:41 +0530

    Time: 3 ms (79.80%) | Memory: 9.7 MB (63.78%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,52 +1,38 @@
 class Solution {
     vector<vector<string>> ans;
+    
+    bool check( int row, int col,vector<string>& res){
+        //check col
 
-    bool check( vector<string>& res, int row, int col){
-        //row check 
-        for( int i=0;i<row;i++)  if( res[i][col]=='Q') return false;
-        //column check
-        // for( int i=0;i<res.size();i++) if( res[row][i]=='Q' && i!=col) return false;
-        int x = row-1, y = col-1 ;
-        while( x>=0 && y>=0) {
-            if(res[x][y]=='Q' ) return false;
-            x--;
-            y--;
-        }
-        x=row-1,y=col+1;
-        while( x>=0 && y<res.size()){
-            if( res[x][y]=='Q') return false;
-            x--;
-            y++;
-        }
+        for( int i=0;i<row;i++) if( res[i][col]=='Q') return false;
+        //left upside
+        for( int i=row-1,j=col-1 ;i>=0 && j>=0 ;i--, j--) if( res[i][j]=='Q') return false;
+
+        //rightside upside
+        for( int i=row-1, j=col+1;i>=0 && j<res[0].size();i--, j++) if( res[i][j]=='Q') return false;
         return true;
     }
 
-    void solve(int row, vector<string>& res){
-        int n = res.size();
-        if( row == n){
-            ans.push_back(res);
+    void solve( int row,vector<string>& res){
+        if( row == res.size()){
+            ans.push_back( res);
             return ;
         }
-        string s(n, '.');
-
-        for( int i=0;i<n;i++){
-            // s[i]='Q';
-            res[row][i]='Q';
-            if( check( res, row, i)){
+        for( int i=0;i<res.size();i++){
+            if( check( row, i, res)){
+                res[row][i] = 'Q';
                 solve( row+1, res);
+                res[row][i]='.';
             }
-            // s[i]='.';
-            res[row][i]='.';
         }
-
     }
-
 public:
     vector<vector<string>> solveNQueens(int n) {
-        if( n==1) return {{"Q"}};
-        else if( n==2 || n==3) return ans;
-        vector<string> res(n,string ( n, '.'));
-        solve( 0, res );
+        string x = "";
+        for( int i=0;i<n;i++) x.push_back('.');
+        vector<string> res( n, x);
+        solve( 0, res);
         return ans;
+
     }
 };
