a4248750b80cfff3d7643a84a6442392c585cf9e
nayanGowdaM <mnayangowda@gmail.com>
2024-06-09 23:56:32 +0530

    Time: 473 ms (20.00%) | Memory: 331.6 MB (60.00%) - LeetSync

--- 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -1,22 +1,37 @@
 class Solution {
-    int dp[2002][4002];
-    int solve( int idx, int r,vector<int>& a){
-        if( idx == a.size()){
-            return 0;
-        }
-        if( dp[idx][r]!=-1) return dp[idx][r];
-        int take =0;
-        if( a[idx]>r){
-            take = a[idx] + solve( idx+1 , r+a[idx] , a);
-        }
-       int notTake = solve( idx+1, r, a);
-        return  dp[idx][r] = max( take, notTake);
-    }
+    // int dp[2002][4002];
+    // int solve( int idx, int r,vector<int>& a){
+    //     if( idx == a.size()){
+    //         return 0;
+    //     }
+    //     if( dp[idx][r]!=-1) return dp[idx][r];
+    //     int take =0;
+    //     if( a[idx]>r){
+    //         take = a[idx] + solve( idx+1 , r+a[idx] , a);
+    //     }
+    //    int notTake = solve( idx+1, r, a);
+    //     return  dp[idx][r] = max( take, notTake);
+    // }
 public:
     int maxTotalReward(vector<int>& reward) {
-        // int n=reward.size();
-        memset( dp, -1, sizeof(dp));
+        int n=reward.size();
+        // memset( dp, -1, sizeof(dp));
         sort( reward.begin(), reward.end());
-        return solve( 0,0, reward);
+        // return solve( 0,0, reward);
+
+        vector<vector<int>> dp( n+1, vector<int>(4002));
+        for( int i=0;i<4002;i++) dp[n][i] = 0;
+
+        for(int i=n-1;i>=0;i--){
+            for( int j=4000;j>=0;j--){
+                int take = 0;
+                if( reward[i] > j){
+                    take = reward[i] + dp[i+1][j+reward[i]];
+                }
+                int notTake = dp[i+1][j];
+                dp[i][j] = max( take , notTake );
+            }
+        }
+        return dp[0][0] ;
     }
 };
