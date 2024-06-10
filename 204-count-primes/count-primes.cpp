80966e0426c9a72aec57fe0791179b89a8909f88
nayanGowdaM <mnayangowda@gmail.com>
2024-06-11 00:07:21 +0530

    Time: 307 ms (64.10%) | Memory: 11.3 MB (97.75%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -5,9 +5,9 @@ public:
         vector<bool> arr( n,true);
         int ct=0;
         arr[0] = arr[1] =false;
-        for( int i=2;i<n;i++){
+        for( int i=2;i*i<n;i++){
             if( arr[i]){
-                for( int j=i+i;j<n;j+=i){
+                for( int j=i*i;j<n;j+=i){
                     arr[j]=false;
                 }
             }
