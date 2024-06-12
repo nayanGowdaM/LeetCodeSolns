9079394a9cd1fb024f4486234fb4eb87411c76a8
nayanGowdaM <mnayangowda@gmail.com>
2024-06-12 09:13:34 +0530

    Time: 15 ms (43.45%) | Memory: 18.9 MB (61.23%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,20 +1,17 @@
 class Solution {
-    int min_ele( vector<int>& ans, int n, int l){
-        int mn=ans[n];
-        for( int i=n+1;i<ans.size() && i<= n+l;i++) mn = min( mn, ans[i]);
-        return mn;
-    }
 public:
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ans( n, INT_MAX);
-        ans[n-1] = 0;
-        for( int i=n-2;i>=0 ;i--){
-            int val = min_ele( ans, i, nums[i]);
-            if(val == INT_MAX) val--;
-            ans[i] = 1 + val;
-            cout<<ans[i]<<endl;
+        int n=nums.size(), l=0,r=0,ct=0;
+        while(r<n-1){
+            int far=0;
+            for( int i=l;i<=r;i++){
+                far = max( far, nums[i] + i);
+            }
+            l=r+1;
+            r=far;
+            ct++;
+
         }
-        return ans[0];
+        return ct;
     }
 };
